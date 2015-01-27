15d8f08054e2f59197afc5dfe6bd960d51b77bde
wallstop <dbfsinfil@gmail.com>
2015-01-26 22:26:43 -0800

    Updated packages

--- Test/DXGameTest/Coverage.bat
+++ Test/DXGameTest/Coverage.bat
@@ -1,5 +1,5 @@
-﻿"..\..\packages\OpenCover.4.5.3427\OpenCover.Console.exe" -target:"..\..\packages\NUnit.Runners.2.6.3\tools\nunit-console.exe" -targetargs:"/nologo DXGameTest.dll /noshadow" -filter:"+[DXGame]DXGame*" -excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" -register:user -output:"CodeCoverageResult.xml"
+﻿"../../packages/OpenCover.4.5.3522/OpenCover.Console.exe" -target:"../../packages/NUnit.Runners.2.6.4/tools/nunit-console.exe" -targetargs:"/nologo DXGameTest.dll /noshadow" -filter:"+[DXGame]DXGame*" -excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" -register:user -output:"CodeCoverageResult.xml"
 
-"..\..\packages\ReportGenerator.2.0.2.0\ReportGenerator.exe" "-reports:CodeCoverageResult.xml" "-targetdir:CodeCoverageReport"
+"../../packages/ReportGenerator.2.0.4.0/ReportGenerator.exe" "-reports:CodeCoverageResult.xml" "-targetdir:CodeCoverageReport"
 
 start file://%~dps0CodeCoverageReport\index.htm

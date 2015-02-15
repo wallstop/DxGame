d650024a15ff8a9558c1779b5a351aa2688abcbc
wallstop <dbfsinfil@gmail.com>
2015-02-15 13:51:47 -0800

    Added Port text to port box in host multiplayer menu

--- Core/Menus/HostMultiplayerMenu.cs
+++ Core/Menus/HostMultiplayerMenu.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using DXGame.Core.Components.Advanced;
+﻿using DXGame.Core.Components.Advanced;
 using DXGame.Core.GraphicsWidgets;
 using DXGame.Core.Input;
+using DXGame.Core.Utils;
 using DXGame.Main;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -13,7 +10,7 @@ namespace DXGame.Core.Menus
 {
     public class HostMultiplayerMenu : Menu
     {
-        public HostMultiplayerMenu(DxGame game) 
+        public HostMultiplayerMenu(DxGame game)
             : base(game)
         {
         }
@@ -22,22 +19,36 @@ namespace DXGame.Core.Menus
         {
             var spriteFont = DxGame.Content.Load<SpriteFont>("Fonts/ComicSans");
 
-            var spatial = (SpatialComponent)
-                new SpatialComponent(DxGame).WithDimensions(new Vector2 {X = 200.0f, Y = spriteFont.LineSpacing + 2 /* wiggle room for cursor */}).WithPosition(600, 500);
+            var portBoxSpatial = (SpatialComponent)
+                new SpatialComponent(DxGame).WithDimensions(new Vector2
+                {
+                    X = 200.0f,
+                    Y = spriteFont.LineSpacing + 2 /* wiggle room for cursor */
+                }).WithPosition(600, 500);
 
             // Ports have a range of 0 - 65536 (2 ^ 16 - 1) -> max length of 5
             var portBox =
-                new TextBox(DxGame).WithSpatialComponent(spatial)
-                    .WithBackGroundColor(Color.White)
-                    .WithTextColor(Color.Black)
-                    .WithMaxLength(5)
+                new TextBox(DxGame).WithSpatialComponent(portBoxSpatial)
+                                   .WithBackGroundColor(Color.White)
+                                   .WithTextColor(Color.Black)
+                                   .WithMaxLength(5)
                     // Only allow numeric values for ports
-                    .WithValidKeys(KeyboardEvent.NumericKeys)
-                    .WithSpriteFont(spriteFont);
+                                   .WithValidKeys(KeyboardEvent.NumericKeys)
+                                   .WithSpriteFont(spriteFont);
 
+            var portLabel =
+                new MenuItem().WithSpriteFont(spriteFont)
+                              .WithText("Port:")
+                              .WithSpace(new Rectangle2f
+                              {
+                                  X = portBoxSpatial.Space.X - /* Pixel Width of "Port:" */ spriteFont.MeasureString("Port:").X,
+                                  Y = portBoxSpatial.Space.Y, 
+                                  Width = portBoxSpatial.Width,
+                                  Height = portBoxSpatial.Height
+                              });
+
+            MenuItems.Add(portLabel);
             DxGame.AddAndInitializeComponent(portBox);
-
-            
 
             base.Initialize();
         }

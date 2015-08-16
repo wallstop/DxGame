6f1c598f259afd8c9cd573cfef5b55da8705f550
wallstop <dbfsinfil@gmail.com>
2015-08-15 22:11:19 -0700

    Added initial (non-tested) RTree

--- Core/Utils/Distance/Distance.cs
+++ Core/Utils/Distance/Distance.cs
@@ -4,5 +4,7 @@ namespace DXGame.Core.Utils.Distance
 {
     public delegate DxVector2 Coordinate<in T>(T point);
 
+    public delegate DxRectangle BoundingBox<in T>(T thing);
+
     public delegate double Distance(DxVector2 lhs, DxVector2 rhs);
 }

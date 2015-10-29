82dbf09c9592ee9957b9457b123ca3794099a9ac
wallstop <dbfsinfil@gmail.com>
2015-10-28 19:32:43 -0700

    Precommit

---
+++ Core/EntityType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXGame.Core
+{
+    [Serializable]
+    [DataContract]
+    public sealed class EntityType : IEquatable<EntityType>
+    {
+        public bool Equals(EntityType other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

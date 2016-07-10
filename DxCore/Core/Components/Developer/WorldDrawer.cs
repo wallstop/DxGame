39246bf4aae5f6b390659a2b2cccfcb020475f98
wallstop <dbfsinfil@gmail.com>
2016-07-09 23:16:58 -0700

    Initial spin on dev WorldDrawer (Farseer)

---
+++ DxCore/Core/Components/Developer/WorldDrawer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DxCore.Core.Components.Basic;
+using DxCore.Core.Models;
+using DxCore.Core.Primitives;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DxCore.Core.Components.Developer
+{
+    public class WorldDrawer : DrawableComponent
+    {
+        public override void Draw(SpriteBatch spriteBatch, DxGameTime gameTime)
+        {
+            WorldModel worldModel = DxGame.Instance.Model<WorldModel>();
+            if(ReferenceEquals(worldModel, null))
+            {
+                
+            }
+        }
+    }
+}

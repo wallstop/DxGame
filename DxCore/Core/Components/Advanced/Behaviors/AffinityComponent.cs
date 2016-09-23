ffaa546e5c9b8664b4c1b85c80ff2471ba174ffb
pixelgruff <pixelgruff@gmail.com>
2016-09-22 22:25:47 -0700

    Added core AI classes

---
+++ DxCore/Core/Components/Advanced/Behaviors/AffinityComponent.cs
@@ -0,0 +1,79 @@
+﻿using DxCore.Core.Components.Basic;
+using DxCore.Core.Messaging;
+using DxCore.Core.Utils;
+using DxCore.Core.Utils.Validate;
+using DXGame.Core.Behaviors;
+using NLog;
+using System.Collections.Generic;
+/**
+* Summarize an entity's behavioral attributes by mapping each commandment (including movement)
+* to a set of attributes and their values
+*/
+namespace DXGame.Core.Components.Advanced.Behaviors
+{
+    public class AffinityComponent : Component
+    {
+        private static readonly Logger LOG = LogManager.GetCurrentClassLogger();
+
+        /**
+         * A single 'affinity' is essentially just a mapping of a single attribute to a score;
+         * we expect each Commandment to map to multiple Attributes
+         */
+        private Dictionary<Commandment, Dictionary<Attribute, Score>> AffinitiesByCommandment { get; }
+
+        private AffinityComponent(Dictionary<Commandment, Dictionary<Attribute, Score>> affinities)
+        {
+            Validate.Hard.IsNotNull(affinities);
+            AffinitiesByCommandment = affinities;
+        }
+
+        // TODO: out values instead of Optionals I guess
+        public Optional<Score> AffinityFor(Commandment commandment, Attribute attribute)
+        {
+            Dictionary<Attribute, Score> affinities; 
+            if (AffinitiesByCommandment.TryGetValue(commandment, out affinities))
+            {
+                Score score;
+                if (affinities.TryGetValue(attribute, out score)) {
+                    return Optional<Score>.Of(score);
+                }
+            }
+            return Optional<Score>.Empty;
+        }
+
+        public static AffinityComponentBuilder Builder()
+        {
+            return new AffinityComponentBuilder();
+        }
+
+        public class AffinityComponentBuilder : IBuilder<AffinityComponent>
+        {
+            private Dictionary<Commandment, Dictionary<Attribute, Score>> AffinitiesByCommandment = new Dictionary<Commandment, Dictionary<Attribute, Score>>();
+
+            public AffinityComponentBuilder WithAffinity(Commandment commandment, Attribute attribute, float score)
+            {
+                return WithAffinity(commandment, attribute, new Score(score));
+            }
+
+            public AffinityComponentBuilder WithAffinity(Commandment commandment, Attribute attribute, Score score)
+            {
+                if (!AffinitiesByCommandment.ContainsKey(commandment))
+                {
+                    // Looks like this is the first entry for this commandment
+                    AffinitiesByCommandment.Add(commandment, new Dictionary<Attribute, Score>());
+                }
+
+                Dictionary<Attribute, Score> commandmentAffinities = AffinitiesByCommandment[commandment];
+                commandmentAffinities[attribute] = score;
+
+                return this;
+            }
+
+            public AffinityComponent Build()
+            {
+                // TODO: Validation
+                return new AffinityComponent(AffinitiesByCommandment);
+            }
+        }
+    }
+}
---
+++ DxCore/Core/Services/Components/GoalAssigner.cs
@@ -0,0 +1,70 @@
+﻿using DxCore.Core.Components.Basic;
+using DxCore.Core.Primitives;
+using DxCore.Core.Utils.Validate;
+using DXGame.Core.Behaviors;
+using DXGame.Core.Behaviors.Goals;
+using DXGame.Core.Components.Advanced.Behaviors;
+using NLog;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DxCore.Core.Components.Advanced.Behaviors
+{
+    /**
+        The Sun Tzu of his time.
+
+        <summary>
+            Determines mappings of BehaviorComponent -> Goals
+        </summary>
+    */
+    class GoalAssigner : Component
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private readonly Dictionary<BehaviorComponent, Behavior> assignments_ = new Dictionary<BehaviorComponent, Behavior>();
+        private readonly IEnumerable<Behavior> candidateBehaviors_;
+
+        public GoalAssigner(IEnumerable<Behavior> candidateBehaviors)
+        {
+            candidateBehaviors_ = candidateBehaviors;
+        }
+
+        /// <summary>
+        /// Extract all BehaviorComponents and assign them the appropriate goals
+        /// </summary>
+        /// <param name="gameTime"></param>
+        protected override void Update(DxGameTime gameTime)
+        {
+            AssignBehaviors();
+        }
+
+        /// <summary>
+        /// Assign behaviors to entities
+        /// </summary>
+        private void AssignBehaviors()
+        {
+            // Extract all BehaviorComponents
+            IEnumerable<BehaviorComponent> behaviorComponents = DxGame.Instance.DxGameElements.OfType<BehaviorComponent>().ToList();
+
+            // For each component (presumably 1:1 with entity)...
+            // TODO: Currently we only assign new goals when old goals have been met.  Implement more complex logic for 'interruptable' and 'non-interruptable' goals/behaviors
+            foreach (BehaviorComponent behaver in behaviorComponents.Where(behaver => !behaver.CurrentGoal.InProgress()))
+            {
+                // Filter all behaviors by constraints...
+                IEnumerable<Behavior> satisfiedBehaviors = candidateBehaviors_.Where(behavior => behavior.SatisfiedFor(behaver, assignments_));
+                Validate.Hard.IsNotEmpty(satisfiedBehaviors, $"No behavior satisfied for {behaver}, not even the null behavior!  (This shouldn't ever happen.)");
+
+                // Sort all remaining behaviors by fitness and take the most fit behavior...
+                Behavior bestBehavior = satisfiedBehaviors.OrderByDescending(behavior => behavior.FitnessFor(behaver, assignments_)).First();
+
+                // Assign, purely to keep track of who is doing what...
+                assignments_[behaver] = bestBehavior;
+
+                // Apply situation-specific logic to acquire a goal, then assign the goal to a the entity's behavior component
+                Goal goal = bestBehavior.ResolveGoalFor(behaver);
+                Logger.Info($"Assigning {goal} to {behaver}.");
+                behaver.AssignGoal(goal);
+            }
+        }
+    }
+}
